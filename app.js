2b40f8191d84d13a9e2e7766519d01c2bc80594a
Leen Barakat <leenbarakat2432001@outlook.com>
2021-08-02 15:28:14 -0700

    add

---
+++ app.js
@@ -0,0 +1,84 @@
+let major = prompt("Do I like my major?")
+if(major !== null){
+    major = major.toLowerCase();
+}
+switch(major){
+    case 'yes':
+        case'y':
+        alert("your answer is wrong")
+        break;
+
+    case 'no':
+        case'n':
+        alert("your answer is right")
+        console.log('your answer is right')
+        break;    
+}
+
+let pasta = prompt("Do I like pasta?")
+if(pasta !== null){
+    pasta = pasta.toLowerCase();
+}
+switch(pasta){
+    case 'yes':
+        case 'y':
+            alert("Your answer is right")
+            console.log('your answer is right')
+            break;
+    
+    case 'no':
+        case 'n':
+            alert("your answer is wrong")
+            break;        
+}
+
+let reading = prompt("Do I read a lot?")
+if(reading !== null){
+    reading = reading.toLowerCase();
+}
+switch(reading){
+    case 'yes':
+        case'y':
+        alert("your answer is wrong")
+        break;
+
+    case 'no':
+        case'n':
+        alert("your answer is right")
+        console.log('your answer is right')
+        break;    
+}
+
+let dancing = prompt("Do I like dancing?")
+if(dancing !== null){
+    dancing = dancing.toLowerCase();
+}
+switch(dancing){
+    case 'yes':
+        case 'y':
+            alert("Your answer is right")
+            console.log('your answer is right')
+            break;
+    
+    case 'no':
+        case 'n':
+            alert("your answer is wrong")
+            break;        
+}
+
+let coding = prompt("Do I like coding?")
+if(coding !== null){
+    coding = coding.toLowerCase();
+}
+switch(coding){
+    case 'yes':
+        case'y':
+        alert("your answer is wrong")
+        break;
+
+    case 'no':
+        case'n':
+        alert("your answer is right")
+        console.log('your answer is right')
+        break;    
+}
